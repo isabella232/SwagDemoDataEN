51d3e6a40d18f4da9eb171d19768639e0c56dcf9
Michael Telgmann <mt@shopware.com>
2023-07-10 12:46:50 +0200

    feat(PT-13122): Change license

--- Bootstrap.php
+++ Bootstrap.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * (c) shopware AG <info@shopware.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
 class Shopware_Plugins_Frontend_SwagDemoDataEN_Bootstrap extends Shopware_Components_Plugin_Bootstrap
 {
     public function getCapabilities()
